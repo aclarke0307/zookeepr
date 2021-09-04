a7465e326b7441369f61920baf7f319551e6a47e
aclar <a.clarke0307@gmail.com>
2021-09-04 18:40:42 -0400

    feature/frontend

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 const { animals } = require('./data/animals');
 const fs = require('fs');
@@ -106,6 +107,18 @@ app.get('/api/animals', (req, res) => {
       const animal = createNewAnimal(req.body, animals);
       res.json(animal);
     }
+  });
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
